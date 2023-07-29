93fd5f92b73475288553f4078d2f212e57d4d878
Wesley da Silva Ribeiro <wesleysilvaribeiro9@gmail.com>
2023-07-29 11:57:15 -0300

    [FEAT] update customer data

--- src/controllers/customers.controller.js
+++ src/controllers/customers.controller.js
@@ -39,17 +39,56 @@ export async function getCustomers(req, res) {
 }
 
 export async function getCustomerById(req, res) {
-  const {id} = req.params;
+  const { id } = req.params;
 
   try {
     const result = await db.query(
       `SELECT id, name, phone, cpf, TO_CHAR(birthday, 'YYYY-MM-DD') AS birthday from customers
-       WHERE id = $1;`, [id]
+       WHERE id = $1;`,
+      [id]
     );
 
     if (result.rowCount === 0) res.sendStatus(404);
 
     res.send(result.rows);
+  } catch (err) {
+    console.log(err.message);
+    return res.status(500).send(err.message);
+  }
+}
+
+export async function editCustomerById(req, res) {
+  const { id } = req.params;
+  const { name, phone, cpf, birthday } = req.body;
+
+  try {
+    // const result = await db.query(
+    //   `SELECT id, name, phone, cpf, TO_CHAR(birthday, 'YYYY-MM-DD') AS birthday from customers
+    //    WHERE id = $1;`, [id]
+    // );
+
+    // if (result.rowCount === 0) res.sendStatus(404);
+
+    const result = await db.query(
+      `
+        SELECT * from customers 
+        WHERE id != $1 
+        AND cpf = $2;`,
+      [id, cpf]
+    );
+    if (result.rowCount > 0)
+      return res
+        .status(409)
+        .send({ message: "Outro cliente com o mesmo CPF já está cadastrado!" });
+
+    await db.query(
+      `UPDATE customers
+        SET name = $2, phone = $3, cpf = $4, birthday = $5
+        WHERE id = $1;`,
+      [id, name, phone, cpf, birthday]
+    );
+
+    res.sendStatus(200);
   } catch (err) {
     console.log(err.message);
     return res.status(500).send(err.message);
--- src/routes/customers.routes.js
+++ src/routes/customers.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { validateCustomerSchema } from "../middlewares/validateCustomerSchema.js";
 import { customerSchema } from "../schemas/customer.schema.js";
-import { createCustomer, getCustomerById, getCustomers } from "../controllers/customers.controller.js";
+import { createCustomer, editCustomerById, getCustomerById, getCustomers } from "../controllers/customers.controller.js";
 
 const customersRouter = Router();
 
 customersRouter.post("/customers", validateCustomerSchema(customerSchema), createCustomer);
 customersRouter.get("/customers", getCustomers);
 customersRouter.get("/customers/:id", getCustomerById);
+customersRouter.put("/customers/:id", validateCustomerSchema(customerSchema), editCustomerById);
 
 export default customersRouter;
